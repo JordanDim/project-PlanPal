cf7ed2db07a7df1db8443f55b81e801ad12b6a7a
JordanDim <dankooo@gmail.com>
2024-05-21 16:17:47 +0300

    add  Profile with simple edit functionality and avatar upload

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "google_api",
@@ -13,6 +14,8 @@ const firebaseConfig = {
   appId: "1:703107405045:web:8cced77b583a037f77e1bc",
 };
 
+
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+export const storage = getStorage(app);
