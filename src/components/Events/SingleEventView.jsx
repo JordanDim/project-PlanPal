eac2c3c4acc4065ca18ea433e57d9e4ff3976a5b
JordanDim <dankooo@gmail.com>
2026-02-04 18:42:35 +0200

    Enhance SingleEventView component: improve participant display with dropdown and adjust layout for better user experience

--- src/components/Events/SingleEventView.jsx
+++ src/components/Events/SingleEventView.jsx
@@ -118,10 +118,69 @@ export default function SingleEventView() {
   if (!event) return <div className="text-center mt-10">No event found.</div>;
 
   return (
-    <div className="single-event-container p-4 max-w-5xl mx-auto rounded-[30px] mt-8 mb-8" style={{ boxShadow: '0 0 30px 0 rgba(0, 0, 0, 0.2)' }}>
+    <div className="single-event-container p-8 max-w-5xl mx-auto rounded-[30px] mt-8 mb-8" style={{ boxShadow: '0 0 30px 0 rgba(0, 0, 0, 0.2)' }}>
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-3xl font-bold">{event.title}</h1>
         <div className="flex items-center space-x-2">
+          <div className="relative" ref={participantsRef}>
+            <details className="dropdown">
+              <summary className="m-1 font-bold py-2 px-4 cursor-pointer btn btn-secondary">
+                Participants
+              </summary>
+              <div
+                className="dropdown-menu absolute w-48 max-h-48 overflow-y-auto mt-2 backdrop-blur-lg bg-white/10 text-black "
+                style={{ zIndex: 999 }}
+              >
+                <ul className="space-y-2 overflow-x-hidden">
+                  {event.peopleGoing ? (
+                    Object.entries(event.peopleGoing).map(
+                      ([participant, participantData], index) => {
+                        const avatar =
+                          participantsAvatars[participant] ||
+                          "https://via.placeholder.com/40";
+                        return (
+                          <li
+                            key={participant}
+                            className={`flex items-center space-x-3 hover:glass ${
+                              index % 3 === 0 && "mt-4"
+                            }`}
+                          >
+                            <img
+                              src={avatar}
+                              alt="Avatar"
+                              className="w-10 h-10 rounded-full"
+                            />
+                            <div>
+                              <div className="font-semibold">
+                                {participantData && participantData.name
+                                  ? participantData.name
+                                  : participant}
+                              </div>
+                              <div className="text-sm">@{participant}</div>
+                            </div>
+                          </li>
+                        );
+                      }
+                    )
+                  ) : (
+                    <li>No participants yet.</li>
+                  )}
+                </ul>
+              </div>
+            </details>
+          </div>
+          {userData.goingToEvents && userData.goingToEvents[event.title] ? (
+            <button className="btn" onClick={() => handleLeaveEvent(event.title)}>
+              Leave Event
+            </button>
+          ) : (
+            <button
+              className="btn btn-primary"
+              onClick={() => handleJoinEvent(event.id, event.title)}
+            >
+              Join Event
+            </button>
+          )}
           <GoBackArrow onClick={() => navigate(`${BASE}events`)} />
           {(userData?.handle === event.creator ||
             userData?.role === "Admin") && (
@@ -146,79 +205,20 @@ export default function SingleEventView() {
       <div className="mb-4">
         <span className="font-semibold">Category:</span> {event.category}
       </div>
-      <div className="mb-4">
-        <span className="font-semibold">Description:</span> {event.description}
-      </div>
       <div className="mb-4 flex">
-        <div className="mr-2 flex justify-start ">
+        <div className="mr-2 flex justify-start">
           <img
             src={event.cover || EVENT_COVER_BY_DEFAULT}
             alt="Event"
-            className="rounded-[30px] max-w-xl h-96 object-contain"
+            className="rounded-[30px] max-w-xl aspect-video object-cover"
           />
         </div>
         <div className="flex-grow ml-2">
           <Map address={event.location} className="w-full h-96" />
         </div>
       </div>
-      <div className="mb-4 relative" ref={participantsRef}>
-        <details className="dropdown">
-          <summary className="m-1 font-bold py-2 px-4 cursor-pointer btn btn-secondary">
-            Participants
-          </summary>
-          <div
-            className="dropdown-menu absolute max-h-48 overflow-y-auto mt-2 backdrop-blur-lg bg-white/10 text-black "
-            style={{ zIndex: 999 }}
-          >
-            <ul className="space-y-2 overflow-x-hidden">
-              {event.peopleGoing ? (
-                Object.entries(event.peopleGoing).map(
-                  ([participant, participantData], index) => {
-                    const avatar =
-                      participantsAvatars[participant] ||
-                      "https://via.placeholder.com/40";
-                    return (
-                      <li
-                        key={participant}
-                        className={`flex items-center space-x-3 hover:glass ${
-                          index % 3 === 0 && "mt-4"
-                        }`}
-                      >
-                        <img
-                          src={avatar}
-                          alt="Avatar"
-                          className="w-10 h-10 rounded-full"
-                        />
-                        <div>
-                          <div className="font-semibold">
-                            {participantData && participantData.name
-                              ? participantData.name
-                              : participant}
-                          </div>
-                          <div className="text-sm">@{participant}</div>
-                        </div>
-                      </li>
-                    );
-                  }
-                )
-              ) : (
-                <li>No participants yet.</li>
-              )}
-            </ul>
-          </div>
-        </details>
-        {userData.goingToEvents && userData.goingToEvents[event.title] ? (
-          <button className="btn" onClick={() => handleLeaveEvent(event.title)}>
-            Leave Event
-          </button>
-        ) : (
-          <button
-            className="btn btn-primary"
-            onClick={() => handleJoinEvent(event.id, event.title)}
-          >
-            Join Event
-          </button>
-        )}
+      <div className="mb-4">
+        <span className="font-semibold">Description:</span> {event.description}
       </div>
     </div>
   );
